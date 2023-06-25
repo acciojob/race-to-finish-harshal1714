44f082ac776c96bf06a59a96488b9e5fa6e2e9c4
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-06-26 00:34:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-
 window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
- const promises = Array.from({ length: 5 }, () => {
-  const delay = Math.floor(Math.random() * 5) + 1; // Random time between 1 and 5 seconds
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`Promise resolved after ${delay} second(s)`);
-    }, delay * 1000);
-  });
-});
+let a=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("a");
+	},3000)
+})
+let b=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("b");
+	},2000)
+})
 
-// Usage example
-const outputElement = document.getElementById("output");
+let c=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("c");
+	},1000)
+})
 
-Promise.any(promises)
-  .then((result) => {
-    outputElement.textContent = result;
-  })
-  .catch((error) => {
-    console.error("All promises rejected:", error);
-  });
+let d=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("d");
+	},4000)
+})
+
+let e=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("e");
+	},5000)
+})
+
+let x= Promise.any([a,b,c,d, e])
+	window.promises=[a,b,c,d,e];
+	x.then((data)=>{
+		const elem= document.getElementById('output');
+		elem.innerText=data;
+	})
